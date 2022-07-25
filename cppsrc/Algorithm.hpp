f449daa521063c19c1e80407f0fe4f866438dc2a
ddf <eef>
2022-07-25 10:07:40 +0800

    leftover attempt to revamp and add simd support

---
+++ cppsrc/Algorithm.hpp
@@ -0,0 +1,14 @@
+//
+//  Algorithm.hpp
+//  274F16NearestSB
+//
+//  Created by Yunlong Liu on 7/23/21.
+//  Copyright © 2021 Yunlong Liu. All rights reserved.
+//
+
+#ifndef Algorithm_hpp
+#define Algorithm_hpp
+
+#include <stdio.h>
+
+#endif /* Algorithm_hpp */
